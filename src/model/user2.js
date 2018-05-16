2725b7d2c311f286275943aadc2f429e76cdb51b
fan zhang <fan.zhang1@nio.com>
2018-05-16 20:28:56 +0800

    test

--- src/model/user2.js
+++ src/model/user2.js
@@ -1,6 +1,5 @@
 module.exports = class extends think.Model {
   async addUser() {
-    console.log(this.name);
     const userId = await this.add({name: 'xxx', email: 'test@gmail.com'});
     return userId;
   }
