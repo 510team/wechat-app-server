01fb42f33f7e12e62a163ecbbbc3a09ed0f96403
xiaoyang.li <xiaoyang.li@nio.com>
2018-08-08 17:17:16 +0800

    ranks

--- src/controller/ranks.js
+++ src/controller/ranks.js
@@ -4,18 +4,23 @@ module.exports = class extends Api {
         const result = {
             success: false,
             errorMsg: ''
-        }
+        };
         const offset = parseInt(this.ctx.param('offset')) || 0;
         const count = parseInt(this.ctx.param('count')) || 10;
-        const data = await this.model('ranks').getRanks(offset, count);
-        think.logger.info(this.ctx.state)
+        const data = await this.model('ranks').getRanks(
+            this.ctx.state.userInfo.openid,
+            offset,
+            count
+        );
+        think.logger.info(this.ctx.state);
         result.data = {
             rankList: data.rankList,
-            user: this.ctx.state.userInfo,
-            amount: data.amount
+            user: this.ctx.state.userInfo.userInfo,
+            amount: data.amount,
+            currentRank: data.rank,
+            score: data.score
         };
         result.success = true;
         return this.json(result);
     }
-}
-;
+};
--- src/model/ranks.js
+++ src/model/ranks.js
@@ -1,11 +1,24 @@
 module.exports = class extends think.Model {
-    async getRanks(offset, count) {
-        const ranks = await this.model('user').join('score ON user.openid=score.openid').order('score DESC').limit(offset, count).select();
-        const score = await this.model('user').join('score ON user.openid=score.openid').select();
+    async getRanks(openid, offset, count) {
+        const rankList = await this.model('user')
+            .join('score ON user.openid=score.openid')
+            .order('score DESC')
+            .limit(offset, count)
+            .select();
+        const scoreData = await this.model('user')
+            .join('score u ON user.openid=u.openid')
+            .where({ 'u.openid': openid })
+            .select();
         const amount = await this.model('user').count('openid');
+        const sql = `SELECT
+                    (SELECT COUNT(1) FROM score vi1 WHERE vi1.score > vi.score) + 1 AS rank,
+                    vi.* FROM score vi WHERE vi.openid = '${openid}'`;
+        const rank = await this.query(sql);
         const data = {
-            rankList: ranks,
-            amount: amount
+            rankList: rankList,
+            amount: amount,
+            rank: rank[0].rank,
+            score: scoreData[0].score
         };
         return data;
     }
